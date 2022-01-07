e6d5c2c31ffb59145f309fbe68bd300afa398dc1
Elijah <ngfei25@gmail.com>
2022-01-08 06:55:27 +0800

    Finished basic endpoint

--- backend/app/models/run.rb
+++ backend/app/models/run.rb
@@ -1,2 +1,10 @@
+require "date"
 class Run < ApplicationRecord
+    scope :filter_min_dist, -> (distance) { where "distance > #{distance}"}
+    scope :filter_max_dist, -> (distance) { where "distance < #{distance}"}
+    scope :filter_started_before, -> (epoch) { where("start_timestamp < ?", Time.at(0, epoch, :millisecond).to_datetime)}
+    scope :filter_started_after, -> (epoch) { where("start_timestamp > ?", Time.at(0, epoch, :millisecond).to_datetime)}
 end
+
+
+# 1.36, 103.81 +- 0.15 0.25
